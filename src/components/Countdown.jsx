0173bc3b2baf8a2393decbddde3d071c2cdb9ecd
Giosepe Luiz <giosepe_luiz3@live.com>
2021-09-22 22:17:52 -0300

    Animação do Chorizo enquanto carrega

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,42 +3,20 @@ import styles from "../styles/Countdown.module.scss";
 import CommunityTitle from "./CommunityTitle";
 
 // Funções necessárias para o funcionamento
-import { padLeft, isPlural } from "../functions/countdownFunctions";
+import { isPlural, countdownTrigger } from "../functions/countdownFunctions";
 
 const Countdown = (props) => {
+  const [loaded, setLoaded] = useState(0);
   const [time, setTime] = useState(null);
   const { endDay, endMonth, endYear } = props;
 
-  useEffect(() => {
-    // Define a data final
-    const countDownDate = new Date(`${endMonth}/${endDay}/${endYear} 00:00:00`).getTime();
-    countDownDate.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+  // Contador regressivo principal
+  useEffect(
+    () => countdownTrigger(endMonth, endDay, endYear, setTime, setLoaded),
+    [endMonth, endDay, endYear, setTime],
+  );
 
-    // Atualiza o contador a cada um segundo
-    const x = setInterval(() => {
-      // Retorna a data e a hora de hoje
-      const now = new Date().getTime();
-      now.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
-
-      // Calcula a diferença entre a data final e agora
-      const distance = countDownDate - now;
-
-      // Cálculo dos dias, minutos e segundos
-      const days = padLeft(Math.floor(distance / (1000 * 60 * 60 * 24)));
-      const hours = padLeft(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const minutes = padLeft(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-      const seconds = padLeft(Math.floor((distance % (1000 * 60)) / 1000));
-
-      // Insere o resultado no State
-      setTime({ days, hours, minutes, seconds });
-
-      // Se o contador acabar, encerra o contador
-      if (distance < 0) {
-        clearInterval(x);
-        setTime(null);
-      }
-    }, 1000);
-  });
+  // Componente principal
   return (
     <div className={styles.countdown}>
       {time && (
@@ -69,7 +47,13 @@ const Countdown = (props) => {
           )}
         </>
       )}
-      {!time && <CommunityTitle title="Comunidade" subtitle="Oficial" />};
+      {!time &&
+        (loaded === -1 ? (
+          <CommunityTitle title="Comunidade" subtitle="Oficial" />
+        ) : (
+          <div className={styles.loading} />
+        ))}
+      ;
     </div>
   );
 };
