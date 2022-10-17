adcf961429857fdeee79f0fac0e9f0c7bd377fdd
Giosepe Luiz <giosepe_luiz3@live.com>
2022-10-17 23:17:40 +0100

    Melhoria nos botões de escutar música.

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -8,15 +8,17 @@ const Song = (props) => {
   const { song } = props;
   return (
     <div className={styles.icons}>
-      {song.map((item) => (
+      {song.map((item: any) => (
         <div className={styles.streaming} key={item.type.toLowerCase()}>
           <Image
             src={`/images/icon-${item.type.toLowerCase()}.svg`}
             width="50"
             height="50"
             alt={`Escute no ${item.type}`}
+            title={`Escute no ${item.type}`}
             onClick={() => gotoUrl(item.url)}
           />
+          <p>{item.type}</p>
         </div>
       ))}
     </div>
